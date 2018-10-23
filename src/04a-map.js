86ed580af80bf9b9ec722d74a94fa16944b6caa8
Yuanqi (Edward) Hong <39491376+Yuanqi-Hong@users.noreply.github.com>
2018-10-23 17:07:53 -0400

    updated map 4

--- src/04a-map.js
+++ src/04a-map.js
@@ -13,29 +13,50 @@ let svg = d3
   .append('g')
   .attr('transform', `translate(${margin.left},${margin.top})`)
 
-let projection = d3_composite.geoAlbersUSA()
+// note it's NOT geoAlbersUSA!
+// when you see "... is not a function"
+// you most likely have spelled it wrong
+let projection = d3.geoAlbersUsa()
 
 let path = d3.geoPath().projection(projection)
 
-let colorScale
+let colorScale = d3.scaleSequential(d3.interpolateRdBu)
+
+let opacityScale = d3.scaleLinear().range([0, 1])
 
 d3.json(require('./data/counties_with_election_data.topojson'))
   .then(ready)
   .catch(err => console.log('Failed on', err))
 
 function ready(json) {
-  console.log('json is', json)
-
   let counties = topojson.feature(json, json.objects.us_counties)
 
   projection.fitSize([width, height], counties)
 
+  colorScale.domain([0, counties.features.length])
+
+  // let marginExtent = d3.extent(
+  //   counties.features,
+  //   d => d.properties.clinton - d.properties.trump
+  // )
+  // colorScale.domain(marginExtent)
+
+  let totalVotesArray = counties.features.map(
+    d => d.properties.clinton + d.properties.trump
+  )
+  let sum = totalVotesArray.reduce((a, b) => a + b, 0)
+
   svg
     .selectAll('.county')
-    .data(counties.arcs)
+    .data(counties.features)
     .enter()
     .append('path')
     .attr('class', 'county')
     .attr('d', path)
-    .attr('fill', '#070707')
+    .attr('fill', d => {
+      if (d.properties.state) {
+        return colorScale(d.properties.clinton - d.properties.trump)
+      }
+    })
+    .attr('opacity', d => opacityScale(d.margin))
 }
--- src/04b-map.js
+++ src/04b-map.js
@@ -2,9 +2,7 @@ import * as d3 from 'd3'
 import * as topojson from 'topojson'
 
 let margin = { top: 0, left: 0, right: 0, bottom: 0 }
-
 let height = 500 - margin.top - margin.bottom
-
 let width = 900 - margin.left - margin.right
 
 let svg = d3
@@ -13,4 +11,41 @@ let svg = d3
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${margin.left},${margin.top})`)
+
+let projection = d3.geoAlbersUsa()
+
+let path = d3.geoPath().projection(projection)
+
+d3.json(require('./data/counties_with_election_data.topojson'))
+  .then(ready)
+  .catch(err => console.log('Failed on', err))
+
+function ready(json) {
+  let counties = topojson.feature(json, json.objects.us_counties)
+
+  projection.fitSize([width, height], counties)
+
+  let totalVotesArray = counties.features.map(
+    d => d.properties.clinton + d.properties.trump
+  )
+  let sum = totalVotesArray.reduce((a, b) => a + b, 0)
+
+  svg
+    .selectAll('.county')
+    .data(counties.features)
+    .enter()
+    .append('path')
+    .attr('class', 'county')
+    .attr('d', path)
+    .attr('fill', d => {
+      if (d.properties.clinton > d.properties.trump) {
+        return 'blue'
+      } else if (d.properties.clinton < d.properties.trump) {
+        return 'red'
+      } else {
+        return '#070707'
+      }
+    })
+    .attr('opacity', d => {})
+}
